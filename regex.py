f589bc5b81de91c0f1ebdf938cfd2dccf09958b9
Jeremy_Hester <west.fl.itguy@mail.com>
2023-08-24 20:15:03 -0400

    added 2nd question

--- regex.py
+++ regex.py
@@ -1,35 +1,35 @@
 import re
 
-pattern=re.compile("(^[A-Z][a-z]+), ([A-Z][a-z]+)?[-]?([A-Z][a-z]+)?")
-pattern2=re.compile("(\B@[a-z]+)+")
-print('''
-      ============================
-          Full Name / Twitter
-      ============================
-      ''')
-with open("names.txt") as f:
-    data = f.readlines()
-    x=0
-    for c in data:
-        name = pattern.findall(c)
-        handle = pattern2.findall(c)
-        if name and handle:
-            if name[0][2] != '':
-                fname = name[0][2]
-                lname1 = name[0][1]
-                lname2 = name[0][0]
-                fullname = f'{fname} {lname1}-{lname2}'
-            else:
-                lname = name[0][0]
-                fname = name[0][1]
-                fullname = f'{fname} {lname}'
+# pattern=re.compile("(^[A-Z][a-z]+), ([A-Z][a-z]+)?[-]?([A-Z][a-z]+)?")
+# pattern2=re.compile("(\B@[a-z]+)+")
+# print('''
+#       ============================
+#           Full Name / Twitter
+#       ============================
+#       ''')
+# with open("names.txt") as f:
+#     data = f.readlines()
+#     x=0
+#     for c in data:
+#         name = pattern.findall(c)
+#         handle = pattern2.findall(c)
+#         if name and handle:
+#             if name[0][2] != '':
+#                 fname = name[0][2]
+#                 lname1 = name[0][1]
+#                 lname2 = name[0][0]
+#                 fullname = f'{fname} {lname1}-{lname2}'
+#             else:
+#                 lname = name[0][0]
+#                 fname = name[0][1]
+#                 fullname = f'{fname} {lname}'
 
-            handle = handle[0]
-            fhandle = f'{handle}'
+#             handle = handle[0]
+#             fhandle = f'{handle}'
             
         
-            print(f'{fullname} / {handle}\n')
-        x+=1
+#             print(f'{fullname} / {handle}\n')
+#         x+=1
 
 
 # (env311) G:\Code Temple\week_4\day1>python -u "g:\Code Temple\week_4\day1\regex.py"
@@ -52,3 +52,27 @@ with open("names.txt") as f:
 
 
 # (env311) G:\Code Temple\week_4\day1>
+
+
+#================================================================================================
+
+
+pattern = re.compile("(^[A-Z][a-z]+) ?([A-Z]?|[A-Z][a-z]+?)? ([A-Z][a-z]+)")
+name_list = []
+
+with open("regex_text.txt") as f:
+    data = f.readlines()
+
+for d in data:
+    name = pattern.findall(d)
+    if name:
+        print(name[0][0], end=" ")
+        if name[0][1]:
+            print(name[0][1], end=" ")
+        if name[0][2]:
+            print(name[0][2])
+    else:
+        print("None")
+    
+   
+
